df0c3944bcb00cc55665215cf59f38a8db3d7652
Junaid Faiz <he630001@gmail.com>
2025-04-28 21:58:49 +0500

    Refactor index.js: standardize code formatting, simplify CORS setup, and improve readability

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,29 @@
-
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config({
-    path: "./.env",
+  path: "./.env",
 });
 import Router from "./src/Routes/Routes.js";
 import db from "./src/Config/ConnectDB.js";
-import cors from 'cors';
-import pkg from 'cloudinary'; 
-const { v2: cloudinary } = pkg; 
-
+import cors from "cors";
+import pkg from "cloudinary";
+const { v2: cloudinary } = pkg;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
 
 const app = express();
 
-
-const whitelist = process.env.whitelist;
-
-const corsOptions = {
-    origin: (origin, callback)=>{
-        if(whitelist.includes(origin) ||!origin){
-            callback(null, true);
-        }else{
-            callback(new Error("Not Allowed By Cors"))
-        }
-    }
-}
-
-app.use(cors(corsOptions));
-app.use(express.json({limit: "50mb"}));
-
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
 
 db();
-app.use("/api",Router)
+app.use("/api", Router);
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log("Server is listening on port " + port);
+  console.log("Server is listening on port " + port);
 });
